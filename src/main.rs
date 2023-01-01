0d81e0c1abccb94c1c5b39812b3f9874a3299c73
Thomas Kline <thomaskline@thomass-imac.myfiosgateway.com>
2022-12-31 23:14:35 -0500

    Updating CI config for rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate actix_web;
 
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
@@ -8,10 +8,7 @@ async fn health_check() -> impl Responder {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new( || {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
         .bind("127.0.0.1:8000")?
         .run()
         .await
