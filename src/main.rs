066133634fbc1ff10cee411e055cd0917fbbabf6
Thomas Kline <thomaskline@thomass-imac.myfiosgateway.com>
2023-01-02 20:40:02 -0500

    Adjusted tests to run off of TcpListener

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate zero2prod;
 
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,14 +1,18 @@
+extern crate tokio;
+
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_works() {
     //Arrange
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in 'reqwest'
     // To perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
     //Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -19,8 +23,13 @@ async fn health_check_works() {
 }
 
 //Launch application in the background somehow
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     let _ = tokio::spawn(server);
+    // We return the application address to the caller
+    format!("http://127.0.0.1:{}", port)
 }
